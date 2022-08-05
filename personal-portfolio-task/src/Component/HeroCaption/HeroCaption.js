27b701a129908857721875504d6dcf071feb861a
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-08-05 21:12:26 +0600

    added FOrward Typing

--- personal-portfolio-task/src/Component/HeroCaption/HeroCaption.js
+++ personal-portfolio-task/src/Component/HeroCaption/HeroCaption.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './HeroCaption.module.scss';
 import background from './Images/backgroundimage.webp';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import OutlineButton from '../Button/OutlineButton';
 import SolidButton from '../Button/SolidButton';
 const HeroCaption = () => {
+
+	useEffect(() => {
+		const texts = ["MD. Khairul Hasan Sajid", "Software Engineer"];
+		let index = 0;
+		let cnt = 0;
+		let currentText = "";
+		let modifiedText = "";
+		
+		const Type = async () => {
+			if (cnt == texts.length) {
+				cnt = 0;
+			}
+			currentText = texts[cnt];
+			modifiedText = currentText.slice(0, ++index);
+			document.getElementById("typed").innerHTML = modifiedText;
+			if (modifiedText.length == currentText.length) {
+				cnt++;
+				index = 0;
+			}
+			await setTimeout(Type, 400);
+
+		};
+		Type();
+
+	}, []);
+
 	return (
 		<Router>		<div className={style.CaptionContainer}>
-			<img className={style.HeroCaptionbackground} src={background} alt="backgorund image" sizes="" srcset="" />
+			<img className={style.HeroCaptionbackground} src={background} alt="backgorund image" sizes="" srcSet="" />
 
 			<div className="container">
 				<div className="row">
 					<div className={`${style.Caption}  col-lg-6 col-md-8 `}>
 						<div className={`${style.HeroCaption}`}>
 							<div> <h3 className={style.CaptionHeading} >Get Every Single Solutions</h3> </div>
-							<div> <h3 className={style.HeroHeading} >I'm MD. Khairul<br /> Hasan Sajid</h3> </div>
+							<div> <h3 className={style.HeroHeading} >I'm <span id="typed">MD. Khairul Hasan Sajid</span></h3> </div>
 							<div> <p className={style.HeroDes}>jhorem rfpsum golor sidt amet, consectetur adipiscing elit, eiusmod tempor incididunt utcjhg labore bet dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p> </div>
 						</div>
 						<div className={style.btnGroup}>
